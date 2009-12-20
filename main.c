9cfed8cc8e7df595b9ba9945210b45090a353b9e
Thomas Eapen <thomas@nowlabs.net>
2009-12-20 17:03:41 -0500

    Converted the functions to take pointers to integers instead of arrays

--- main.c
+++ main.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int	arr[10] = {3,6,1,2,3,8,4,1,7,2};
-void bubble(int a[], int N);
-int compare(int m, int n);
+void bubble(int *p, int N);
+int compare(int *m, int *n);
 
 int main (int argc, const char * argv[]) {
 	int i;
@@ -18,19 +18,19 @@ int main (int argc, const char * argv[]) {
     return 0;
 }
 
-void bubble(int a[], int N) {
+void bubble(int *p, int N) {
 	int i, j, t;
 	for (i = N - 1; i >= 0; i--) {
 		for (j = 1; j <= i; j++) {
-			if (compare(a[j-1], a[j])) {
-				t = a[j - 1];
-				a[j - 1] = a[j];
-				a[j] = t;
+			if (compare(&p[j-1], &p[j])) {
+				t = p[j - 1];
+				p[j - 1] = p[j];
+				p[j] = t;
 			}
 		}
 	}
 }
 
-int compare(int m, int n) {
-	return (m > n);
+int compare(int *m, int *n) {
+	return (*m > *n);
 }
